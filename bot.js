e5ccdcb10b5c8ac6e49333135e7911f537fedef8
Rewin <125676315+Rewin31@users.noreply.github.com>
2023-02-23 22:27:11 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const { spawn } = require('child_process');
 const https = require('https');
 
@@ -6,7 +7,7 @@ const client = new Discord.Client();
 const PREFIX = '!';
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}! Made By Rewin`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async (message) => {
@@ -15,30 +16,51 @@ client.on('message', async (message) => {
   const args = message.content.slice(PREFIX.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === 'api') {
-    if (args.length < 4) {
-      message.channel.send('Error: Invalid arguments');
-      return;
-    }
-    const host = args[0];
-    const port = args[1];
-    const time = args[2];
-    const method = args[3];
-    const url = `https://api/api/api.php?username=rewin55&key=rewin55&host=${host}&port=${port}&time=${time}&method=${method}`;
+if (command === 'help') {
+  const helpEmbed = new Discord.MessageEmbed()
+    .setColor('#099ff')
+    .setTitle('Command List')
+    .setDescription('List of available commands')
+    .addField('!ping', 'Usage: !ping.')
+    .addField('!ssh', 'Usage: !ssh host time its will be sending command too your ssh.')
+    .addField('!api2', 'Usage: !api. Displays a message with a link to an API documentation.')
+    .addField('!adsoyad', 'Usage: !adsoyad [ad] [soyad]. Retrieves information from an API and saves it to a file.')
+    .addField('!api', 'Usage: !api. Displays a message with a link to an API documentation.')
+    .addField('!ban', 'Usage: !ban [@username]. Bans the specified user from using the bot.');
 
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        console.log(`API response: ${data}`);
-        message.channel.send(`API response: ${data}`);
-      });
-    }).on('error', (err) => {
-      console.error(`API request error: ${err}`);
-      message.channel.send(`API request error: ${err}`);
+  message.channel.send(helpEmbed);
+
+} else if (command === 'adsoyad') {
+  if (args.length < 1) {
+    message.channel.send('Error: Invalid arguments');
+    return;
+  }
+  const ad = args[0];
+  const soyad = args[1];
+  const url = `https://api.com/?ad=$(ad}&soyad=${soyad}`;
+
+  https.get(url, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
     });
+    res.on('end', () => {
+      console.log(`API response: ${data}`);
+      fs.writeFile('info.txt', data, (err) => {
+        if (err) {
+          console.error(`Error writing to file: ${err}`);
+          message.channel.send(`Error writing to file: ${err}`);
+        } else {
+          console.log('File written successfully!');
+          const attachment = new Discord.MessageAttachment('./info.txt');
+          message.channel.send('info Çıkarıldı:', attachment);
+        }
+      });
+    });
+  }).on('error', (err) => {
+    console.error(`API request error: ${err}`);
+    message.channel.send(`API request error: ${err}`);
+  });
   } else if (command === 'api2') {
     if (args.length < 4) {
       message.channel.send('Error: Invalid arguments');
@@ -48,7 +70,7 @@ client.on('message', async (message) => {
     const port = args[1];
     const time = args[2];
     const method = args[3];
-    const url = `https://api/&key=rewin55&host=${host}&port=${port}&time=${time}&method=udp`;
+    const url = `https://api.com/&key=rewin55&host=${host}&port=${port}&time=${time}&method=udp`;
 
     https.get(url, (res) => {
       let data = '';
@@ -118,4 +140,4 @@ client.on('message', async (message) => {
   }
 });
 
-client.login('MTA3Njk5NzI5OTAzODg0NzAxNg.GLAEzB.dC_WKjIPLhgoJRBOLVhGVOw6foGDpiyMY3ft0Y');
+client.login('yourbottoken');
